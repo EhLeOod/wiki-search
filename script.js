c91dee6258e05f1873023e782476af15ff0594a5
Eliud Uresti <urestieliud@gmail.com>
2017-09-03 18:28:27 -0600

    moving pc's

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
+  var entry;
   $('#random').on('click', function() {
    var random = 'https://en.wikipedia.org/wiki/Special:Random';
 
    window.open(random);
   });
+//FIX THIS.
+  function wikiAPI() {
+    //WORKS. Means that wikiAPI() IS running. alert('hello world');
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm&formatversion=2&continue=',
+      // query.pages.title<?>
 
-});    
+      //maybe want to access extracts|info|pageprops|revisions in parameters of prop (query sub/module) e.g. prop:info<?>
+      // dataType:'json',
+      // type:'GET',
+      
+      success: function(data) {
+        entry = data.query.revisions
+        ('#entry').text(entry);
+            } 
+      });
+  }
+
+wikiAPI();  
+
+});
+
+
 
 // <!-- 
 
