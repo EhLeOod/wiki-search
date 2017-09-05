9b146fa0372a89b0714b5be89a6407f33077fc7d
Eliud Uresti <urestieliud@gmail.com>
2017-09-04 18:01:05 -0600

    CORS fkn works. I think.

--- script.js
+++ script.js
@@ -5,34 +5,40 @@ $(document).ready(function(){
 
    window.open(random);
   });
-//FIX THIS.
   function wikiAPI() {
-    //WORKS. Means that wikiAPI() IS running. alert('hello world');
-    $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm&formatversion=2&continue=',
-      // query.pages.title<?>
-
-      //maybe want to access extracts|info|pageprops|revisions in parameters of prop (query sub/module) e.g. prop:info<?>
-      // dataType:'json',
-      // type:'GET',
-      
-      success: function(data) {
-        entry = data.query.revisions
-        ('#entry').text(entry);
-            } 
-      });
-  }
+        //WORKS. Means that wikiAPI() IS running.
+    alert('hello world');
+      $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm&formatversion=2&continue=',
+        type: 'GET',
+        data: {
+          'origin': '*',
+        },
+        processData: true,
+        // headers: {
+        //   'Access-Control-Allow-Origin' : '*'
+        // } ,
+        crossDomain: true,
+        // async: false
+        // jsonp: 'jsonp',
+        // dataType: 'jsonp',
+        // accepts: 'text/html'
+        // jsonpCallback:'response'
+        // query.pages.title<?>
+  
+        //maybe want to access extracts|info|pageprops|revisions in parameters of prop (query sub/module) e.g. prop:info<?>
+        // dataType:'json',
+        // type:'GET',
+        
+        success: function(response) {
+          alert('fuck you CORS light');
+          
+              } 
+        });
+    }
+   
+  
 
 wikiAPI();  
 
 });
-
-
-
-// <!-- 
-
-// User Story: I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
-
-// DONEUser Story: I can click a button to see a random Wikipedia entry.
-
-// Hint #2: Here's an entry on using Wikipedia's API: https://www.mediawiki.org/wiki/API:Main_page.
